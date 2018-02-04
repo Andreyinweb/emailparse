0f33d78f9662b8ccc2d74ceec1f09e451adf49d9
AndreyInWeb <andreyinweb@gmail.com>
2018-02-04 13:55:47 +0200

    2 save 2018.02.04

--- config_parse.py
+++ config_parse.py
@@ -1,8 +1,8 @@
-# site = 'example.com' # Site address for search. Адрес сайта для поиска
+site = 'example.com' # Site address for search. Адрес сайта для поиска
 
 TIMEPAUS = 2 # Number of seconds between requests. Количество секунд между запросами
 maximum = 0 # Number of iterations. Количество итераций
 MAX = 100 # Number of pages. Количество страниц
 TIMEMAX = 15.0 # Maximum execution time of the program. Максимальное время выполнения программы
 
-site = 'gordonua.com'
+# site = 'gordonua.com'
